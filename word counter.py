ab85419e341a53263abc9ca1df5af58bd55fded2
hisidharth <hisidharth2014@gmail.com>
2021-06-28 15:43:38 -0700

    Update word counter.py

--- word counter.py
+++ word counter.py
@@ -1,4 +1,4 @@
-example_paragraph = "ha ha ha ha ha L9 L9 L9 L9 XD XD XD XD XD"
+example_paragraph = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
 example_paragraph_lower = example_paragraph.lower()
 
 example_word_list = example_paragraph_lower.split(" ")
@@ -26,11 +26,11 @@ def sort(dic):
     return sorted_dic
 print(example_paragraph_lower)
 frequency_dictionary = counter(example_word_list)
-print(frequency_dictionary)
+##print(frequency_dictionary)
 frequency_dictionary=rev(frequency_dictionary)
-print(frequency_dictionary)
+##print(frequency_dictionary)
 frequency_dictionary= sort(frequency_dictionary)
-print(frequency_dictionary)
+##print(frequency_dictionary)
 answer=input("What word would you like to know the frequency of?")
 z=counter(example_word_list)
 if answer in z:
